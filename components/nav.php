24440d927dddc58a3da64c0d7d4283bee1a7edb3
kanyasrithong <ks4264@drexel.edu>
2026-02-18 13:04:03 -0500

    Update nav links

--- components/nav.php
+++ components/nav.php
@@ -4,6 +4,6 @@
   </a>
   <a <?php echo ($page === 'sku_management') ? "class='nav-button active'" : "class='nav-button'"?> href="sku_management.php">SKU MANAGEMENT</a>
   <a <?php echo ($page === 'internal_inventory') ? "class='nav-button active'" : "class='nav-button'"?> href="internal_inventory.php">INVENTORY</a>
-  <a <?php echo ($page === 'mpl') ? "class='nav-button active'" : "class='nav-button'"?> href="mpl_records.php">MPL RECORDS</a>
-  <a <?php echo ($page === 'order') ? "class='nav-button active'" : "class='nav-button'"?> href="order_records.php">ORDER RECORDS</a>
+  <a <?php echo ($page === 'mpl_records') ? "class='nav-button active'" : "class='nav-button'"?> href="mpl_records.php">MPL RECORDS</a>
+  <a <?php echo ($page === 'order_records') ? "class='nav-button active'" : "class='nav-button'"?> href="order_records.php">ORDER RECORDS</a>
 </nav>
