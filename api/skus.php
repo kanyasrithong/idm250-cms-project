8e81c43d4f74cc4425e18e05fe4a0866358ef133
kanyasrithong <ks4264@drexel.edu>
2026-02-21 16:33:50 -0500

    Add SKUs GET API

--- api/skus.php
+++ api/skus.php
@@ -2,17 +2,28 @@
   header('Content-Type: application/json');
   header('Access-Control-Allow-Origin: *');
 
-  require_once('../db.php');
+  require_once __DIR__ . '/../db.php';
+  require __DIR__ . '/../functions/wms.php';
 
   $method = $_SERVER['REQUEST_METHOD'];
+  $id = isset($_SERVER['PATH_INFO']) ? intval(ltrim($_SERVER['PATH_INFO'], '/')) : 0;
 
   if ($method === 'GET') {
+    $skus = get_skus();
 
-    echo json_encode(['success' => true, 'message' => 'Hello World!']);
-  
-  } elseif ($method === 'POST') {
-  
-  } else {
-    http_response_code(405);
-    echo json_encode(['error' => 'Method not allowed']);
+    if ($id > 0) :
+      $sku = get_sku($id);
+
+      if ($sku) {
+        echo json_encode(['success' => true, 'data' => $sku]);
+      } else {
+        http_response_code(404);
+
+        echo json_encode(['error' => 'Product not found', 'message' => 'Number of available SKUs: ' .  count($skus)]);
+        exit();
+      }
+    else :
+      http_response_code(201);
+      echo json_encode(['success' => true, 'data' => $skus]);
+    endif;
   }
