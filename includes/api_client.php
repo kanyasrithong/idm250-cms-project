465cdd6d3d7b58827a7eeb3a61ad44fa8c89fa89
kanyasrithong <ks4264@drexel.edu>
2026-03-13 12:43:38 -0400

    test api key

--- includes/api_client.php
+++ includes/api_client.php
@@ -34,7 +34,7 @@
 function notify_cms_mpl_confirmed($reference_number) {
     global $env;
 
-    $url = $env['CMS_API_URL'] . '/mpls.php';
+    $url = 'https://digmstudents.westphal.drexel.edu/cp3282/idm250/api/mpls.php';
     $api_key = $env['CMS_API_KEY'];
 
     $data = [
@@ -48,7 +48,7 @@ function notify_cms_mpl_confirmed($reference_number) {
 function notify_cms_order_shipped($order_number, $shipped_at) {
     global $env;
 
-    $url = $env['CMS_API_URL'] . '/orders.php';
+    $url = 'https://digmstudents.westphal.drexel.edu/cp3282/idm250/api/orders.php';
     $api_key = $env['CMS_API_KEY'];
 
     $data = [
