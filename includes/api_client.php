753cfb7b2e6cbac8c9da3f63392544e18b03b9e6
kanyasrithong <ks4264@drexel.edu>
2026-03-13 13:41:03 -0400

    See error attempt 2

--- includes/api_client.php
+++ includes/api_client.php
@@ -22,7 +22,7 @@
     $status_code = intval($matches[1] ?? 0);
 
     if ($status_code < 200 || $status_code >= 300) {
-        file_put_contents(__DIR__ . '/debug.txt', "Status: $status\nResponse: $response");
+        file_put_contents(__DIR__ . '/../debug.txt', "Status: $status\nResponse: $response");
         return null;
     }
 
